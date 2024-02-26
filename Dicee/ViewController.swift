17bc76d8684c833c9eeeab058654eba2792f8670
slv <xout.online@gmail.com>
2024-02-26 13:55:18 +0200

    1

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var number:Int = 0
-    var leftDiceNumber:Int = 1
-    var rightDiceNumber:Int = 5
+    var countRolls:Int = 0
+    var leftDiceNumber:Int = 0
+    var rightDiceNumber:Int = 0
     @IBOutlet weak var LlabelNumber: UILabel!
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     //1
     //2
     //3
-    
+    	
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,33 +28,44 @@ class ViewController: UIViewController {
       
         
     }
-    @IBAction func restartDices(_ sender: UIButton) {
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceTwo")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceThree")
-    }
     
     
     @IBAction func buttonFlipp(_ sender: UIButton) {
-        number += 1
-        LlabelNumber.text = "You rolled " + String(number) + " time/s"
-        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"),
-                                UIImage(imageLiteralResourceName: "DiceTwo"),
-                                UIImage(imageLiteralResourceName: "DiceThree"),
-                                UIImage(imageLiteralResourceName: "DiceFour"),
-                                UIImage(imageLiteralResourceName: "DiceFive"),
-                                UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
+        countRolls += 1
         
-        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"),
+        
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),
                                 UIImage(imageLiteralResourceName: "DiceTwo"),
                                 UIImage(imageLiteralResourceName: "DiceThree"),
                                 UIImage(imageLiteralResourceName: "DiceFour"),
                                 UIImage(imageLiteralResourceName: "DiceFive"),
-                                UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
-        rightDiceNumber = rightDiceNumber - 1
+                                UIImage(imageLiteralResourceName: "DiceSix")]
+
+        leftDiceNumber = Int.random(in: 0...5)
+        diceImageView1.image = diceArray[leftDiceNumber]
+        
+        rightDiceNumber = Int.random(in: 0...5)
+        diceImageView2.image = diceArray[rightDiceNumber]
+        
+        if leftDiceNumber==rightDiceNumber{
+            LlabelNumber.text = "Congrats!!! You get a pair of \(rightDiceNumber+1)"
+        }
+        else{
+            LlabelNumber.text = "You rolled " + String(countRolls) + " time/s"
+        }
     }
     
+
     
+    
+    @IBAction func restartDices(_ sender: UIButton) {
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceOne")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceOne")
+        countRolls = 0
+        leftDiceNumber = 1
+        rightDiceNumber = 5
+        LlabelNumber.text = "Let`s roll them UP!"
+    }
 }
 
 
